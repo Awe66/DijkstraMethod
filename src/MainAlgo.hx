2ad2576d74a73f465498bab9efb0555b2cf587c8
rowrowape <zewwws@gmail.com>
2015-06-16 22:49:02 +0700

    Determinant
    Specifies the type of token

--- src/MainAlgo.hx
+++ src/MainAlgo.hx
@@ -1,7 +1,10 @@
 package src;
 import graphic.WriterToConsole;
+import parseLogic.Algo;
 import reader.ReaderFromConsole;
+import src.graphic.Writer;
 import types.Symbol;
+import reader.Reader;
 
 /**
  * ...
@@ -10,13 +13,19 @@ import types.Symbol;
 class MainAlgo
 {
     private var currentVariable:Symbol; //
-	Reader reader = new ReaderFromConsole();
-	Writer writer = new WriterToConsole();
-	
+	var reader: Reader = new ReaderFromConsole();
+	var writer: Writer = new WriterToConsole();
+	var algo: Algo = new Algo(writer);
 	public function new() 
 	{
 		currentVariable = reader.readNext();
-		if currentVariable
-	}
+		if (currentVariable.getValue().charAt(0) < '9' && currentVariable.getValue().charAt(0) > '0') 
+		{
+			writer.add(currentVariable);
+		} else 
+		
+		{
+			algo.nextStep(currentVariable);
+		}
 	
 }
