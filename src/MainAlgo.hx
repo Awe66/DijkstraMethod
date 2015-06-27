a8c4b1afe131aa429bd9fa5d694cf8a15c1f5777
Awe66 <dart.fk712@gmail.com>
2015-06-27 15:40:44 +0600

    SALU v1

--- src/MainAlgo.hx
+++ src/MainAlgo.hx
@@ -3,8 +3,8 @@ import graphic.WriterToConsole;
 import openfl.display.Sprite;
 import openfl.events.KeyboardEvent;
 import parseLogic.ParseAlgo;
-import reparseLogic.ReparseAlgo;
 import reader.ReaderFromString;
+import reparseLogic.ReparseAlgo;
 import src.graphic.Writer;
 import graphic.WriterToView;
 import types.Symbol;
@@ -16,20 +16,23 @@ import reader.Reader;
  */
 class MainAlgo extends Sprite
 {
-    private var currentVariable:Symbol; 
+    private var currentVariable:Symbol; //
 	var reader: Reader;
 	var writer: Writer;
-	var parseAlgo: ParseAlgo;
+	var algo: ParseAlgo;
 	var reparseAlgo: ReparseAlgo;
-	
+	var salu:Bool = false;
 	private var listOfSymbols:List<Symbol>;
 	public function new(reader:Reader, writer:Writer) 
 	{
 		super();
 		this.reader = reader;
 		this.writer = writer;
-		parseAlgo = new ParseAlgo(writer);
+		algo = new ParseAlgo(writer);
+		reparseAlgo = new ReparseAlgo(writer);
+		addChild(reparseAlgo);
 		listOfSymbols = new List<Symbol>();
+		
 		writer.printSignature();
 	}
 	
@@ -57,21 +60,29 @@ class MainAlgo extends Sprite
 	
 	public function nextStep()
 	{
-		trace('Enter pressed');
-		if (listOfSymbols.isEmpty()) {
-			parseAlgo.end();
+		if (listOfSymbols.isEmpty() && !salu) {
+			algo.end();
+			listOfSymbols = writer.outputList;
+			salu = true;
+			var bufSymbol:Symbol;
 			return;
 		}
-		
-		currentVariable = listOfSymbols.pop();
-		
-		if (!currentVariable.isOperator()) 
-		{
-			writer.addToOutput(currentVariable);
-		} else 
-		{
-			parseAlgo.nextStep(currentVariable);
+		if(!salu){
+			currentVariable = listOfSymbols.pop();
+			
+			if (!currentVariable.isOperator()) 
+			{
+				writer.addToOutput(currentVariable);
+			} else 
+			{
+				algo.nextStep(currentVariable);
+			}
+		} else {
+			currentVariable = listOfSymbols.pop();
+			reparseAlgo.nextStep(currentVariable);	
 		}
-		
+		if (salu && listOfSymbols.isEmpty()) {
+			return;
+		}
 	}
 }
